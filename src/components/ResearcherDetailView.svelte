732b60da3f1344a83ed39b858f749ae117dd8946
Jon Saad-Falcon <jonsaadfalcon@gatech.edu>
2020-03-23 18:54:44 -0400

    Framework for Researcher DetailView and QueryView

--- src/components/ResearcherDetailView.svelte
+++ src/components/ResearcherDetailView.svelte
@@ -1,1 +1,20 @@
-<p>DetailView goes here</p>
+<article class="message is-info">
+  <div class="message-header">
+    <p>Researcher Information</p>
+  </div>
+  <a class="panel-block">
+    <p class="has-text-weight-bold"> Name: </p>
+  </a>
+  <a class="panel-block">
+    <p class="has-text-weight-bold"> Affiliation: </p>
+  </a>
+  <a class="panel-block">
+    <p class="has-text-weight-bold"> Google Scholar Keywords: </p>
+  </a>
+  <a class="panel-block">
+    <p class="has-text-weight-bold"> Citations: </p>
+  </a>
+  <a class="panel-block">
+    <p class="has-text-weight-bold"> URL: </p>
+  </a>
+</article>
--- src/components/ResearcherQueryView.svelte
+++ src/components/ResearcherQueryView.svelte
@@ -1,1 +1,49 @@
-<p>Query View goes here</p>
+<nav class="panel">
+  <p class="panel-heading">
+    Research Topic Search
+  </p>
+  <div class="panel-block">
+    <p class="control has-icons-left">
+      <input class="input" type="text" placeholder="Search">
+      <span class="icon is-left">
+        <i class="fas fa-search" aria-hidden="true"></i>
+      </span>
+    </p>
+  </div>
+  <a class="panel-block">
+    <span class="panel-icon">
+      <i class="fas fa-book" aria-hidden="true"></i>
+    </span>
+    Artificial Intelligence
+  </a>
+  <a class="panel-block">
+    <span class="panel-icon">
+      <i class="fas fa-book" aria-hidden="true"></i>
+    </span>
+    Natural Language Processing
+  </a>
+  <a class="panel-block">
+    <span class="panel-icon">
+      <i class="fas fa-book" aria-hidden="true"></i>
+    </span>
+    Computer Vision
+  </a>
+  <a class="panel-block">
+    <span class="panel-icon">
+      <i class="fas fa-book" aria-hidden="true"></i>
+    </span>
+    Human-Computer Interaction
+  </a>
+  <a class="panel-block">
+    <span class="panel-icon">
+      <i class="fas fa-book" aria-hidden="true"></i>
+    </span>
+    Deep Learning
+  </a>
+  <a class="panel-block">
+    <span class="panel-icon">
+      <i class="fas fa-book" aria-hidden="true"></i>
+    </span>
+    Machine Learning
+  </a>
+</nav>
