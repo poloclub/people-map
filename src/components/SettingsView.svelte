7068ecc75ddbb923a0bbcd8993bb538b16aa8064
Jon Saad-Falcon <jonsaadfalcon@gatech.edu>
2020-03-25 19:55:27 -0400

    Minor UI Edits

--- src/components/SettingsView.svelte
+++ src/components/SettingsView.svelte
@@ -1,15 +1,16 @@
 <style>
         .columns{
             padding-left: 40px;
-            background: white;
+            background: #D3D3D3;
             border: 1px solid grey;
         }
         .column{
           background: #DDA0DD; 
           border-radius: 3px;
-          padding-top: 23px;
+          padding-top: 10px;
+          padding-bottom: 10px;
           width: 200px;
-          height: 100px;
+          height: 70px;
           margin-top: 10px;
           margin-bottom: 10px;
           margin-right: 10px;
@@ -28,28 +29,29 @@
 <div class="columns">
   <div class="column" >
       <h2 class="text is-size-6 has-text-weight-bold" style="padding-bottom:0px;">Keywords Included</h2>
-      <input id="sliderWithValue" class="slider has-output svelte-1v4uv99" min="0" max="15" step="1" type="range" style="margin-top: 0px;outline: none;border-top-width: 0px;border-right-width: 0px;border-left-width: 0px;border-bottom-width: 0px;">
-      <output for="sliderWithValue" style="top:0px;">1</output>
+      <input id="sliderWithValue" class="slider has-output svelte-1v4uv99" min="0" max="15" step="1" type="range" style="margin-top: 0px;outline: none;border-top-width: 0px;border-right-width: 0px;border-left-width: 0px;border-bottom-width: 0px; 
+      margin-bottom: 0px;">
+      <output for="sliderWithValue" style="top:0px; background: grey;">1</output>
   </div>
   <div class="column">
       <h2 class="text is-size-6 has-text-weight-bold" style="padding-bottom:0px;"> Top Choices</h2>
       <input id="sliderWithValue" class="slider has-output svelte-1v4uv99" min="0" max="15" step="1" type="range" style="margin-top: 0px;outline: none;border-top-width: 0px;border-right-width: 0px;border-left-width: 0px;border-bottom-width: 0px;">
-      <output for="sliderWithValue" style="top:0px;">1</output>
+      <output for="sliderWithValue" style="top:0px; background: grey;">1</output>
   </div>
-  <div class="column" style="background: white;">
+  <div class="column" style="background: #D3D3D3;">
 
   </div>
   <div class="column">
       <h2 class="text is-size-6 has-text-weight-bold" style="padding-bottom:0px;">Keywords Added</h2>
       <input id="sliderWithValue" class="slider has-output svelte-1v4uv99" min="0" max="15" step="1" type="range" style="margin-top: 0px;outline: none;border-top-width: 0px;border-right-width: 0px;border-left-width: 0px;border-bottom-width: 0px;">
-      <output for="sliderWithValue" style="top:0px;">1</output>
+      <output for="sliderWithValue" style="top:0px; background: grey;">1</output>
   </div>
   <div class="column">
       <h2 class="text is-size-6 has-text-weight-bold" style="padding-bottom:0px; border: 0px;">Clusters</h2>
       <input id="sliderWithValue" class="slider has-output svelte-1v4uv99" min="0" max="15" step="1" type="range" style="margin-top: 0px;outline: none;border-top-width: 0px;border-right-width: 0px;border-left-width: 0px;border-bottom-width: 0px;">
-      <output for="sliderWithValue" style="top:0px;">1</output>
+      <output for="sliderWithValue" style="top:0px; background: grey;">1</output>
   </div>
-  <div class="column">
+  <div class="column" >
       <div class="field">
         <h2 class="text is-size-6 has-text-weight-bold" style="padding-bottom:5px; ">Display Names</h2>
         <input id="switchRtlExample" type="checkbox" name="switchRtlExample" 
@@ -57,13 +59,13 @@
         <label for="switchRtlExample" style="padding-left: 25px;"></label>
       </div>
   </div>
-  <div class="column" style="background: white;">
+  <div class="column" style="background: #D3D3D3;">
 
   </div>
-  <div class="column" style="background: white; ">
+  <div class="column" style="background: #D3D3D3; ">
       
   </div>
-  <div class="column" style="background: white;">
+  <div class="column" style="background: #D3D3D3;">
       
   </div>
 </div>
